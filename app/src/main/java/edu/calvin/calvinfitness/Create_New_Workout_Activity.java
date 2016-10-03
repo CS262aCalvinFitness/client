4df8d9b120d97a8becb11524c4b94836f1d67ffb
Mitch Stark <mitchstark@dhcp85-3.calvin.edu>
2016-10-03 10:09:46 -0400

    reworked the gitignore to now ignore all .idea files

--- app/src/main/java/edu/calvin/calvinfitness/Create_New_Workout_Activity.java
+++ app/src/main/java/edu/calvin/calvinfitness/Create_New_Workout_Activity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 
+/*
+This activity gives the user the option to create a new workout. This activity shows up when the
+"Create New Workout" button is clicked from the MainActivity.
+ */
 public class Create_New_Workout_Activity extends AppCompatActivity {
 
     @Override
--- app/src/main/java/edu/calvin/calvinfitness/See_Previous_Workouts_Activity.java
+++ app/src/main/java/edu/calvin/calvinfitness/See_Previous_Workouts_Activity.java
@@ -4,6 +4,10 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*
+This activity shows the user the previous workouts that they have completed. This activity is shown
+when the "See Previous Workouts" button is clicked on MainActivity.
+ */
 public class See_Previous_Workouts_Activity extends AppCompatActivity {
 
     @Override
