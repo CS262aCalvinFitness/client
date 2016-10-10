d4e7a185b3bdedd54883790d55d57d2d62c59a08
Ethan Clark <ethanclark@Ethans-MacBook-Pro-4.local>
2016-10-10 19:11:18 -0400

    Adding About Menus

--- app/src/main/java/edu/calvin/calvinfitness/MainActivity.java
+++ app/src/main/java/edu/calvin/calvinfitness/MainActivity.java
@@ -4,12 +4,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /*
 This is the entry point of the app. It enters into the Main Activity, giving three options for
 where to go, as well as a welcome label.
  */
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     /*called when user clicks on start_workout_button
     * @param View - the view that has been clicked
-    * @return - void */
+    * @return - void
+    */
     public void StartWorkout(View view){
         Intent intent = new Intent(this, Start_Workout_Activity.class);
         startActivity(intent);
@@ -26,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
 
     /*called when the user clicks on the create_Workout_button
     * @param View - the view that has been clicked
-    * @return - void */
+    * @return - void
+    */
     public void CreateNewWorkout(View view){
         Intent intent = new Intent(this, Create_New_Workout_Activity.class);
         startActivity(intent);
@@ -34,9 +39,39 @@ public class MainActivity extends AppCompatActivity {
 
     /*called when the user clicks on the past_workout_button
     * @param View - the view that has been clicked
-    * @return - void */
+    * @return - void
+    */
     public void PastWorkout(View view){
         Intent intent = new Intent(this, See_Previous_Workouts_Activity.class);
         startActivity(intent);
+    }
+
+    /**
+     * onCreateOptionsMenu creates the menu at the top of the page layout
+     *
+     * @param: menu
+     * @return: true
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_page_about, menu);
+        return true;
+    }
+
+    /* onOptionsItemSelected performs an action if an menu item is selected
+     *
+     * @param: item
+     * @return: true -> if About item is clicked
+     *          super.onCreateItemsSelected(item) -> otherwise
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home_page_about:
+                startActivity(new Intent(getApplicationContext(), HomePageAboutActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/edu/calvin/calvinfitness/See_Previous_Workouts_Activity.java
+++ app/src/main/java/edu/calvin/calvinfitness/See_Previous_Workouts_Activity.java
@@ -3,6 +3,8 @@ package edu.calvin.calvinfitness;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 /*
 This activity shows the user the previous workouts that they have completed. This activity is shown
@@ -16,5 +18,34 @@ public class See_Previous_Workouts_Activity extends AppCompatActivity {
         setContentView(R.layout.activity_see__previous__workouts_);
         Intent intent = getIntent();
         setTitle("View Past Workout Results");
+    }
+
+    /**
+     * onCreateOptionsMenu creates the menu at the top of the page layout
+     *
+     * @param: menu
+     * @return: true
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_page_about, menu);
+        return true;
+    }
+
+    /* onOptionsItemSelected performs an action if an menu item is selected
+     *
+     * @param: item
+     * @return: true -> if About item is clicked
+     *          super.onCreateItemsSelected(item) -> otherwise
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home_page_about:
+                startActivity(new Intent(getApplicationContext(), ViewPastWorkoutAboutActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
