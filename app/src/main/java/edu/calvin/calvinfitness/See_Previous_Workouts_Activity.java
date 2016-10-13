cd116ba954a58f4bfd7311572187c27c7d3f23b4
Ethan Clark <ethanclark@dhcp82-169.calvin.edu>
2016-10-13 18:37:32 -0400

    Updated home page layout and spinner items

--- app/src/main/java/edu/calvin/calvinfitness/See_Previous_Workouts_Activity.java
+++ app/src/main/java/edu/calvin/calvinfitness/See_Previous_Workouts_Activity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 /*
 This activity shows the user the previous workouts that they have completed. This activity is shown
@@ -12,12 +14,27 @@ when the "See Previous Workouts" button is clicked on MainActivity.
  */
 public class See_Previous_Workouts_Activity extends AppCompatActivity {
 
+    //Define Variables
+    private Spinner past_workout_spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_see__previous__workouts_);
         Intent intent = getIntent();
         setTitle("View Past Workout Results");
+
+        past_workout_spinner = (Spinner) findViewById(R.id.previous_result_spinner);
+
+        //Adapter for past workouts spinner
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,R.array.past_workouts_array, android.R.layout.simple_spinner_item
+        );
+
+        // Assign the dropdown items in the past workouts spinner
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        past_workout_spinner.setAdapter(adapter);
+        past_workout_spinner.setSelection(0);
     }
 
     /**
