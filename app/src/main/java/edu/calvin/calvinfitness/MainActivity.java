49aa0dbee2e87ccbefcdacf2ca4a2815c1a18b41
Mitch Stark <mitchstark@Mitchs-MacBook-Pro-2.local>
2016-10-31 12:27:35 -0400

    remove testing code

--- app/src/main/java/edu/calvin/calvinfitness/MainActivity.java
+++ app/src/main/java/edu/calvin/calvinfitness/MainActivity.java
@@ -31,12 +31,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        List<Workout> workoutList = new Workout_Reader().read(this);
-        for(Workout w : workoutList) {
-            System.out.println(w.getWorkout_name());
-        }
-
     }
 
     /*called when user clicks on start_workout_button
