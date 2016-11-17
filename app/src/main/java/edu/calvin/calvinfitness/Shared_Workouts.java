3b41264049ef6a68ddd7e5fe432fe2bfe8e0d662
Mitch Stark <mitchstark@Mitchs-MacBook-Pro-2.local>
2016-11-17 09:32:06 -0500

    create shared workout sheet

---
+++ app/src/main/java/edu/calvin/calvinfitness/Shared_Workouts.java
@@ -0,0 +1,13 @@
+package edu.calvin.calvinfitness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Shared_Workouts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared__workouts);
+    }
+}
