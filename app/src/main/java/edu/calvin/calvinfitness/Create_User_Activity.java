2add8fa7b0acaf2afff58401f9e141a9a3bb8205
MattKotva <mdk22@students.calvin.edu>
2016-11-21 15:30:13 -0500

    Committing to master login activities

---
+++ app/src/main/java/edu/calvin/calvinfitness/Create_User_Activity.java
@@ -0,0 +1,13 @@
+package edu.calvin.calvinfitness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Create_User_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create__user_);
+    }
+}
